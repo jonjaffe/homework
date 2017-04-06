70d0002bd0146effa5a2f8d99b9c2ed6b8c93727
Jon Jaffe <jonjaffe@Jons-MacBook-Pro.local>
2017-04-06 07:23:25 -0700

    Finishes big o

---
+++ w2d4/big_octopus.rb
@@ -0,0 +1,70 @@
+def sluggish(fishes)
+  fishes.each_with_index do |fish1, i1|
+    max_length = true
+
+    fishes.each_with_index do |fish2, i2|
+      next if i1 == i2
+      max_length = false if fish2.length > fish1.length
+    end
+
+    return fish1 if max_length
+  end
+
+end
+
+class Array
+  def merge_sort(&prc)
+    prc ||= Proc.new { |x, y| x <=> y }
+    return self if self.length <= 1
+
+    mid = self.length / 2
+    sorted_left = self.take(mid).merge_sort(&prc)
+    sorted_right = self.drop(mid).merge_sort(&prc)
+
+    Array.merge(sorted_left, sorted_right, &prc)
+  end
+
+  def self.merge(left, right, &prc)
+    merged = []
+
+    until left.empty? || right.empty?
+      prc.call(left.first, right.first) <= 0 ? merged << left.shift : merged << right.shift
+    end
+
+    merged + left + right
+  end
+end
+
+def linear_biggest_fish(fishes)
+  biggest_fish = fishes.first
+
+  fishes.each do |fish|
+    if fish.length > biggest_fish.length
+      biggest_fish = fish
+    end
+  end
+
+  biggest_fish
+
+end
+
+def slow_dance(direction, tiles_array)
+  tiles_array.each_with_index do |tile, index|
+    return index if tile == direction
+  end
+end
+
+tiles_hash = {
+    "up" => 0,
+    "right-up" => 1,
+    "right"=> 2,
+    "right-down" => 3,
+    "down" => 4,
+    "left-down" => 5,
+    "left" => 6,
+    "left-up" => 7
+}
+
+def fast_dance(direction, tiles_hash)
+  tiles_hash[direction]
+end
